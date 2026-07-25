6185162465671737b29c8abbc2cf8885877a4541
mirkobrombin <brombin94@gmail.com>
2026-07-25 16:20:22 +0200

    fix: fetch sinty-timedate from the org

--- package/sinty-timedate/sinty-timedate.mk
+++ package/sinty-timedate/sinty-timedate.mk
@@ -1,5 +1,5 @@
 SINTY_TIMEDATE_VERSION = v0.1.0
-SINTY_TIMEDATE_SITE = https://github.com/mirkobrombin/sinty-timedate/releases/download/$(SINTY_TIMEDATE_VERSION)
+SINTY_TIMEDATE_SITE = https://github.com/singularityos-lab/sinty-timedate/releases/download/$(SINTY_TIMEDATE_VERSION)
 SINTY_TIMEDATE_SOURCE = sinty-timedate-$(SINTY_TIMEDATE_VERSION)-x86_64.tar.gz
 SINTY_TIMEDATE_LICENSE = NOT-SPECIFIED
 
