66e54bf78d415e6b46633aaccb0b164a07f442c5
mirkobrombin <brombin94@gmail.com>
2026-07-31 11:50:16 +0200

    build: update system components for r7

--- package/libsingularity/libsingularity.mk
+++ package/libsingularity/libsingularity.mk
@@ -6,7 +6,7 @@
 
 # Local dev builds from buildroot-build/local.mk _OVERRIDE_SRCDIR (build dir -custom),
 # which supersedes this github SITE. Keep the pin for RC/CI (no override present).
-LIBSINGULARITY_VERSION = 4dcaa14f16de51a9c8b1e0ac9ab8de96973d1b73
+LIBSINGULARITY_VERSION = e2f5fa6b723dd8590f95255e3dc5f17ffe856f5b
 LIBSINGULARITY_SITE = $(call github,singularityos-lab,libsingularity,$(LIBSINGULARITY_VERSION))
 LIBSINGULARITY_LICENSE = GPL-3.0+
 LIBSINGULARITY_LICENSE_FILES = LICENSE
--- package/sinty-sdb/sinty-sdb.mk
+++ package/sinty-sdb/sinty-sdb.mk
@@ -1,4 +1,4 @@
-SINTY_SDB_VERSION = 0d1dc6b8e3dd592b51cb873754bb91e6b206e70c
+SINTY_SDB_VERSION = f5c6942cc81c344c77c27b7c2fc84cbe2d38b633
 SINTY_SDB_SITE = $(call github,singularityos-lab,sinty-sdb,$(SINTY_SDB_VERSION))
 SINTY_SDB_LICENSE = GPL-3.0-only
 SINTY_SDB_GOMOD = github.com/singularityos-lab/sinty-sdb
